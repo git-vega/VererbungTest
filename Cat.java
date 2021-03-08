babb1ca17b905ded6550caae63ef0242e93d0eed
Michael Rezza Vega <rezzamichael@gmail.com>
2021-03-08 21:09:47 +0100

    vererbung catdog

---
+++ Cat.java
@@ -0,0 +1,17 @@
+
+public class Cat {
+	//Attribute
+	String name;
+	int age;
+	String color;
+	String favFood;
+	
+	//Methoden
+	public void sleep() {
+		System.out.println("Ich schlafe gerade...");
+	}
+	
+	public void purr() {
+		System.out.println("Schnurr Schnurr...");
+	}
+}
---
+++ Dog.java
@@ -0,0 +1,17 @@
+
+public class Dog {
+	//Attribute
+	String name;
+	int age;
+	String color;
+	String favFood;
+	
+	//Methoden
+	public void sleep() {
+		System.out.println("Ich schlafe gerade...");
+	}
+	
+	public void bark() {
+		System.out.println("Wau");
+	}
+}
